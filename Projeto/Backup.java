4bb8abc1c17a6e0a36e7be37e5d61639f64cfdfe
Pedro Noevo <up201604725@fe.up.pt>
2020-03-11 11:18:12 +0000

    Clened code

--- Projeto/Backup.java
+++ Projeto/Backup.java
@@ -1,46 +1,37 @@
 import java.io.*;
 import java.net.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.nio.file.Files;
-import java.util.Arrays;
-import java.security.MessageDigest;
-import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDDocumentInformation;
-import java.io.File;
+import java.security.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 class Backup {
 
     int MAX_CHUNK_SIZE = 64000;
-    private byte[] pdf;
+    private byte[] file;
+    private BasicFileAttributes attributes;
     private byte[][] chunks;
     private String fileId;
-    private File pdfFile;
     private String filePath = "./pdfTest/1.pdf";
 
     private void readFile() {
-        Path pdfPath = Paths.get(this.filePath);
-        this.pdfFile = new File(this.filePath);
+        Path file = Paths.get(this.filePath);
         try {
-            this.pdf = Files.readAllBytes(pdfPath);
+            this.file = Files.readAllBytes(file);
+            this.attributes = Files.readAttributes(file, BasicFileAttributes.class);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
     private void createChunks() {
-        int chunksN = (this.pdf.length / MAX_CHUNK_SIZE) + 1;
+        int chunksN = (this.file.length / MAX_CHUNK_SIZE) + 1;
         this.chunks = new byte[chunksN][MAX_CHUNK_SIZE];
 
         for (int i = 0; i < chunksN; i++) {
             for (int j = 0; j < MAX_CHUNK_SIZE; j++) {
-                if (j + ((i + 1) * MAX_CHUNK_SIZE) >= this.pdf.length)
+                if (j + ((i + 1) * MAX_CHUNK_SIZE) >= this.file.length)
                     break;
-                this.chunks[i][j] = this.pdf[j + ((i + 1) * MAX_CHUNK_SIZE)];
+                this.chunks[i][j] = this.file[j + ((i + 1) * MAX_CHUNK_SIZE)];
             }
         }
 
@@ -49,37 +40,21 @@ class Backup {
     private void createHash() {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            PDDocument pdfDocument = PDDocument.load(this.pdfFile);
-            PDDocumentInformation info = pdfDocument.getDocumentInformation();
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
-            if (String.valueOf(pdfDocument.getNumberOfPages()) != null)
-                outputStream.write(String.valueOf(pdfDocument.getNumberOfPages()).getBytes());
-            if (info != null) {
-                if (info.getTitle() != null)
-                    outputStream.write(info.getTitle().getBytes());
-                if (info.getAuthor() != null)
-                    outputStream.write(info.getAuthor().getBytes());
-                if (info.getSubject() != null)
-                    outputStream.write(info.getSubject().getBytes());
-                if (info.getKeywords() != null)
-                    outputStream.write(info.getKeywords().getBytes());
-                if (info.getCreator() != null)
-                    outputStream.write(info.getCreator().getBytes());
-                if (info.getProducer() != null)
-                    outputStream.write(info.getProducer().getBytes());
-                if (info.getCreationDate() != null)
-                    outputStream.write(info.getCreationDate().toString().getBytes());
-                if (info.getModificationDate() != null)
-                    outputStream.write(info.getModificationDate().toString().getBytes());
-                if (info.getTrapped() != null)
-                    outputStream.write(info.getTrapped().getBytes());
-            }
-            outputStream.write(this.pdf);
+            if (this.attributes.isOther())
+                outputStream.write("true".getBytes());
+            if (this.attributes.isRegularFile())
+                outputStream.write("true".getBytes());
+            if (this.attributes.isSymbolicLink())
+                outputStream.write("true".getBytes());
+            outputStream.write(this.attributes.creationTime().toString().getBytes());
+            outputStream.write(this.attributes.lastModifiedTime().toString().getBytes());
 
-            byte pdfToHash[] = outputStream.toByteArray();
+            outputStream.write(this.file);
 
-            this.fileId = digest.digest(pdfToHash).toString();
+            byte fileToHash[] = outputStream.toByteArray();
+            this.fileId = digest.digest(fileToHash).toString();
         } catch (NoSuchAlgorithmException | IOException ex) {
             ex.printStackTrace();
         }
--- Projeto/runClient.sh
+++ Projeto/runClient.sh
@@ -12,7 +12,7 @@ mkdir "$BIN_DIR"
  
 # Start compilation
 
-javac -cp ".:./lib/pdfbox-2.0.19.jar:./lib/commons-logging-1.2.jar" -d "$BIN_DIR" *.java 
+javac -d "$BIN_DIR" *.java 
 
 #run
-java -cp ".:./lib/pdfbox-2.0.19.jar:./lib/commons-logging-1.2.jar:./bin/client" "$FILE_NAME" "$@";
+java -cp "./bin/client" "$FILE_NAME" "$@";
